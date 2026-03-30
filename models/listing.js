4ea1874f4f1b351fa344d0914de835d9669adbe0
Siya Saxena <saxenasiya9@gmail.com>
2026-03-30 21:32:03 +0530

    major-project

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description:String,
+    image:{
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://plus.unsplash.com/premium_photo-1711305682256-3b1874c923bd?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3Mnx8fGVufDB8fHx8fA%3D%3D",
+            set: (v) => v === "" 
+                ? "https://plus.unsplash.com/premium_photo-1711305682256-3b1874c923bd?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3Mnx8fGVufDB8fHx8fA%3D%3D" 
+                : v,
+        }
+    },
+    price: Number,
+    location:String,
+    country: String,
+
+    
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
